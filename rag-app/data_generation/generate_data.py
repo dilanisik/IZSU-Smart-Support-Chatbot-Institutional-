77f0e2b06e7b137ddb2d41cebbcfe80b8cb84ac8
Dilan <163019915+dilanisik@users.noreply.github.com>
2026-08-07 12:21:44 +0300

    Refactor data generation and add seasonal factors
    Expanded clients and water_consumption tables, now there are 500 clients and 12000 records for 24 months of each client. Total of 25000 records with receipts table.

--- rag-app/data_generation/generate_data.py
+++ rag-app/data_generation/generate_data.py
@@ -2,10 +2,25 @@ from faker import Faker
 import random
 import json
 from datetime import date, timedelta
+import pandas as pd
 
 from sqlalchemy import create_engine, text
 from config import settings
 from logging_config import get_logger
+from dateutil.relativedelta import relativedelta
+
+def mevsim_carpani(ay_numarasi: int) -> float:
+    if ay_numarasi in (6, 7, 8):        # yaz -> tüketim artar
+        return random.uniform(1.3, 1.6)
+    elif ay_numarasi in (12, 1, 2):     # kış -> tüketim azalır
+        return random.uniform(0.7, 0.9)
+    else:                                # ilkbahar/sonbahar -> normal
+        return random.uniform(0.95, 1.1)
+
+def outlier_carpani() -> float:
+    if random.random() < 0.03:   # %3 ihtimalle
+        return random.uniform(3.0, 6.0)
+    return 1.0    
 
 IZMIR_ILCELERI = ["Konak", "Karşıyaka", "Bornova", "Buca", "Çiğli", "Gaziemir", "Bayraklı", "Güzelbahçe", "Narlıdere", "Balçova", "Karabağlar", "Menemen", "Aliağa", "Foça", "Seferihisar",
                    "Urla", "Dikili", "Bergama", "Kınık", "Ödemiş", "Tire", "Selçuk", "Menderes", "Torbalı", "Kemalpaşa", "Sasalı", "Karaburun", "Çeşme", "Beydağ", "Bayındır", "Kiraz", "Menemen", "Aliağa"]
@@ -17,7 +32,7 @@ Faker.seed(42)
 random.seed(42)
 
 musteriler = []
-for i in range(100):
+for i in range(500):
     musteriler.append({
         "ad": fake.first_name(),
         "soyad": fake.last_name(),
@@ -27,8 +42,12 @@ for i in range(100):
         "kayit_tarihi": fake.date_between(start_date="-5y", end_date="today"), #5 yıl öncesine kadar fake date
     })
 
+df_musteriler = pd.DataFrame(musteriler)
+df_musteriler.to_csv("musteriler.csv" , index = False, encoding="utf-8-sig")
+print("musteriler.csv kaydedildi")
+
 adresler = []
-for i in range(100):
+for i in range(500):
     adresler.append({
         "il": "İzmir",
         "ilce": random.choice(IZMIR_ILCELERI),
@@ -40,24 +59,30 @@ for i in range(100):
     })
 
 su_tuketimi = []
-for i in range(100):
-    ilk_endeks = round(random.uniform(100, 5000), 2)       # başlangıç sayaç değeri
-    tuketim = round(random.uniform(5.0, 50.0), 2)           # bu ay ne kadar su tüketildi
-    son_endeks = round(ilk_endeks + tuketim, 2)              # son_endeks >= ilk_endeks garantili
-
-    su_tuketimi.append({
-        "ilk_endeks": ilk_endeks,
-        "son_endeks": son_endeks,
-        "tuketim_m3": tuketim,
-        "okuma_tarihi": fake.date_between(start_date="-5y", end_date="today"),
-    })
+for musteri in musteriler:
+    ilk_endeks = round(random.uniform(100, 5000), 2)       #başlangıç sayaç değeri
+    baslangic_tarihi = musteri["kayit_tarihi"]
+    for ay in range(24): #iç döngü: o müşterinin 24 ayı
+        okuma_tarihi = baslangic_tarihi + relativedelta(months=ay)  # her ayın aynı günü
+        taban_tuketim = random.uniform(5.0, 25.0)
+        tuketim = round(taban_tuketim * mevsim_carpani(okuma_tarihi.month) * outlier_carpani(), 2)
+        son_endeks = round(ilk_endeks + tuketim, 2)  
+        
+    
+        su_tuketimi.append({
+          "ilk_endeks": ilk_endeks,
+          "son_endeks": son_endeks,
+          "tuketim_m3": tuketim,
+          "okuma_tarihi": okuma_tarihi,
+        })
+        ilk_endeks=son_endeks
 
 
 faturalar = []
-for i in range(100):
-    tuketim = su_tuketimi[i]["tuketim_m3"]
-    okuma_tarihi = su_tuketimi[i]["okuma_tarihi"]
-    birim_fiyat = 25.50  # TL/m3 -- sabit bir varsayım
+for tuketim_kaydı in su_tuketimi:
+    tuketim = tuketim_kaydı["tuketim_m3"]
+    okuma_tarihi = tuketim_kaydı["okuma_tarihi"]
+    birim_fiyat = 25.50  # TL/m3 -varsayım
 
     faturalar.append({
         "donem": okuma_tarihi.replace(day=1),          
@@ -66,12 +91,10 @@ for i in range(100):
         "son_odeme_tarihi": okuma_tarihi + timedelta(days=20),
         "odendi_mi": random.choices([True, False], weights=[75, 25])[0],  # BOOLEAN
     })
+    
+print(len(faturalar))
 
 
-print(json.dumps(musteriler, indent=2, ensure_ascii=False, default=str))
-print(json.dumps(adresler, indent=2, ensure_ascii=False, default=str))
-print(json.dumps(su_tuketimi, indent=2, ensure_ascii=False, default=str))
-print(json.dumps(faturalar, indent=2, ensure_ascii=False, default=str))
 
 ##################################################################
 
@@ -113,13 +136,14 @@ with engine.begin() as conn:  # begin() = otomatik transaction, hata olursa heps
     # 3. su_tuketimi INSERT 
     tuketim_id_map = []  # index i -> gerçek tuketim_id
     for i, a in enumerate(su_tuketimi):
+        musteri_index = i //24  #hangi musteri oldugunu anlamak icin
         result = conn.execute(
             text("""
                 INSERT INTO su_tuketimi (musteri_id, ilk_endeks, son_endeks, tuketim_m3, okuma_tarihi)
                 VALUES (:musteri_id, :ilk_endeks, :son_endeks, :tuketim_m3, :okuma_tarihi   )
                 RETURNING tuketim_id
             """),
-            {**a, "musteri_id": musteri_id_map[i]} ####en önemli satır, bağlama
+            {**a, "musteri_id": musteri_id_map[musteri_index]} ####en önemli satır, bağlama
          )
         tuketim_id = result.scalar()
         tuketim_id_map.append(tuketim_id)  #tuketim_id_map listesine ekleme
@@ -129,11 +153,12 @@ with engine.begin() as conn:  # begin() = otomatik transaction, hata olursa heps
     
     # 4. faturalar INSERT 
     for i, f in enumerate(faturalar):
+        musteri_index = i//24 #hangi musteri oldugunu anlamak icin
         conn.execute(
             text("""
                 INSERT INTO faturalar (musteri_id, tuketim_id, donem, tuketim_m3, tutar, son_odeme_tarihi, odendi_mi)
                 VALUES (:musteri_id, :tuketim_id, :donem, :tuketim_m3, :tutar, :son_odeme_tarihi, :odendi_mi)
             """),
-            {**f, "musteri_id": musteri_id_map[i], "tuketim_id": tuketim_id_map[i]  } ####en önemli satır, bağlama
+            {**f, "musteri_id": musteri_id_map[musteri_index], "tuketim_id": tuketim_id_map[i]  } ####en önemli satır, bağlama, i kalıyor cunku 12000 elemanlı ve su_tuketimi ile birebir aynı sırada
         )
     logger.info("Faturalar eklendi: %d kayit", len(faturalar))
